d80505d18ff17bcbb8646c5782a0d15f81c75483
17110 <1711042219@qq.com>
2023-06-08 14:41:26 +0800

    ltsf-linears done

--- Linears.py
+++ Linears.py
@@ -1,27 +1,130 @@
 import torch
 from torch import nn
 
-class LSTFLinear(nn.Module):
-    def __init__(self,input_size,output_size,sensors=0,individual=False):
-        super(LSTFLinear, self).__init__()
-        self.input_size=input_size
-        self.output_size=output_size
-        self.sensors=sensors
-        self.individual=individual
-        self.linear=None
-        if individual:
-            self.linear=nn.ModuleList()
-            for i in range(sensors):
-                self.linear.append(nn.Linear(input_size,output_size))
-        else:
-            self.linear=nn.Linear(input_size,output_size)
 
-    def forward(self,x):
+class LTSFLinear(nn.Module):
+    def __init__(self, input_size, output_size, sensors=0, individual=False):
+        super(LTSFLinear, self).__init__()
+        self.input_size = input_size
+        self.output_size = output_size
+        self.sensors = sensors
+        self.individual = individual
+        self.linear = None
+        if individual:
+            self.linear = nn.ModuleList()
+            for i in range(sensors):
+                self.linear.append(nn.Linear(input_size, output_size))
+        else:
+            self.linear = nn.Linear(input_size, output_size)
+
+    def forward(self, x):
         if self.individual:
-            output = torch.zeros([x.size(0),self.output_size,x.size(2)],dtype=x.dtype).to(x.device)
+            output = torch.zeros([x.size(0), self.output_size, x.size(2)], dtype=x.dtype).to(x.device)
             for i in range(self.sensors):
-                output[:,:,i] = self.linear[i](x[:,:,i])
+                output[:, :, i] = self.linear[i](x[:, :, i])
             x = output
         else:
-            x = self.linear(x.permute(0,2,1)).permute(0,2,1)
+            x = self.linear(x.permute(0, 2, 1)).permute(0, 2, 1)
         return x
+
+
+class LTSFNLinear(nn.Module):
+    def __init__(self, input_size, output_size, sensors=0, individual=False):
+        super(LTSFNLinear, self).__init__()
+        self.input_size = input_size
+        self.output_size = output_size
+        self.sensors = sensors
+        self.individual = individual
+        self.linear = None
+        if individual:
+            self.linear = nn.ModuleList()
+            for i in range(sensors):
+                self.linear.append(nn.Linear(input_size, output_size))
+        else:
+            self.linear = nn.Linear(input_size, output_size)
+
+    def forward(self, x):
+        seq_last = x[:, -1:, :].detach()
+        x = x - seq_last
+        if self.individual:
+            output = torch.zeros([x.size(0), self.output_size, x.size(2)], dtype=x.dtype).to(x.device)
+            for i in range(self.sensors):
+                output[:, :, i] = self.linear[i](x[:, :, i])
+            x = output
+        else:
+            x = self.linear(x.permute(0, 2, 1)).permute(0, 2, 1)
+        x = x + seq_last
+        return x  # [Batch, Output length, Channel]
+
+class MovingAvg(nn.Module):
+    """
+    Moving average block to highlight the trend of time series
+    """
+    def __init__(self, kernel_size, stride):
+        super(MovingAvg, self).__init__()
+        self.kernel_size = kernel_size
+        self.avg = nn.AvgPool1d(kernel_size=kernel_size, stride=stride, padding=0)
+
+    def forward(self, x):
+        # padding on the both ends of time series
+        front = x[:, 0:1, :].repeat(1, (self.kernel_size - 1) // 2, 1)
+        end = x[:, -1:, :].repeat(1, (self.kernel_size - 1) // 2, 1)
+        x = torch.cat([front, x, end], dim=1)
+        x = self.avg(x.permute(0, 2, 1))
+        x = x.permute(0, 2, 1)
+        return x
+
+
+class SeriesDecomp(nn.Module):
+    """
+    Series decomposition block
+    """
+    def __init__(self, kernel_size):
+        super(SeriesDecomp, self).__init__()
+        self.moving_avg = MovingAvg(kernel_size, stride=1)
+
+    def forward(self, x):
+        moving_mean = self.moving_avg(x)
+        res = x - moving_mean
+        return res, moving_mean
+
+class LTSFDLinear(nn.Module):
+    """
+    Decomposition-Linear
+    """
+    def __init__(self, input_size,output_size,sensors=0,individual=False,kernel_size=25):
+        super(LTSFDLinear, self).__init__()
+        self.input_size = input_size
+        self.output_size = output_size
+
+        self.decompsition = SeriesDecomp(kernel_size)
+        self.individual = individual
+        self.sensors = sensors
+
+        if self.individual:
+            self.linear_seasonal = nn.ModuleList()
+            self.linear_trend = nn.ModuleList()
+
+            for i in range(self.channels):
+                self.linear_seasonal.append(nn.Linear(self.input_size, self.output_size))
+                self.linear_trend.append(nn.Linear(self.input_size, self.output_size))
+        else:
+            self.linear_seasonal = nn.Linear(self.input_size, self.output_size)
+            self.linear_trend = nn.Linear(self.input_size, self.output_size)
+
+    def forward(self, x):
+        # x: [Batch, Input length, Channel]
+        seasonal_init, trend_init = self.decompsition(x)
+        seasonal_init, trend_init = seasonal_init.permute(0,2,1), trend_init.permute(0,2,1)
+        if self.individual:
+            seasonal_output = torch.zeros([seasonal_init.size(0), seasonal_init.size(1), self.output_size], dtype=seasonal_init.dtype).to(seasonal_init.device)
+            trend_output = torch.zeros([trend_init.size(0), trend_init.size(1), self.output_size], dtype=trend_init.dtype).to(trend_init.device)
+            for i in range(self.channels):
+                seasonal_output[:,i,:] = self.linear_seasonal[i](seasonal_init[:, i, :])
+                trend_output[:,i,:] = self.linear_trend[i](trend_init[:, i, :])
+        else:
+            seasonal_output = self.linear_seasonal(seasonal_init)
+            trend_output = self.linear_trend(trend_init)
+
+        x = seasonal_output + trend_output
+        return x.permute(0,2,1) # to [Batch, Output length, Channel]
--- main.py
+++ main.py
@@ -7,21 +7,24 @@ import random
 import torch
 from torch.utils.data import DataLoader
 from Datapreprocessor import Datapreprocessor, TSDataset
-from Linears import LSTFLinear
+from Linears import LTSFLinear, LTSFNLinear, LTSFDLinear
 from tqdm import tqdm
 import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-b', '--batch_size', type=int, default=1024)
+parser.add_argument('-B', '--best_model', action='store_true')
 parser.add_argument('-C', '--CUDA_VISIBLE_DEVICES', type=str, default='0,1,2,3,4,5,6,7')
-parser.add_argument('-d', '--dataset', type=str, default='wht')
+parser.add_argument('-d', '--dataset', type=str, default='wht', help='wht, gweather')
 parser.add_argument('-e', '--total_eopchs', type=int, default=20)
+parser.add_argument('-f', '--fixed_seed', type=int, default=None)
 parser.add_argument('-G', '--gpu', action='store_true')
 parser.add_argument('-I', '--individual', action='store_true')
-parser.add_argument('-i', '--input_len', type=int, default=60)
+parser.add_argument('-i', '--input_len', type=int, default=336)
+parser.add_argument('-k', '--kernel_size', type=int, default=25)
 parser.add_argument('-l', '--lr', type=float, default=.001)
 parser.add_argument('-m', '--model', type=str, default='linear', help='linear, dlinear, nlinear')
-parser.add_argument('-o', '--output_len', type=int, default=24)
+parser.add_argument('-o', '--output_len', type=int, default=96)
 parser.add_argument('-s', '--stride', type=int, default=1)
 parser.add_argument('-S', '--save_dir', type=str, default='save')
 parser.add_argument('-t', '--train_ratio', type=float, default=.6)
@@ -41,17 +44,21 @@ individual = args.individual
 lr = args.lr
 which_model = args.model
 save_dir = args.save_dir
+fixed_seed = args.fixed_seed
+best_model = args.best_model
+kernel_size = args.kernel_size
 
 if not os.path.exists(save_dir):
     os.makedirs(save_dir)
 
 os.environ['CUDA_VISIBLE_DEVICES'] = args.CUDA_VISIBLE_DEVICES
-device = torch.device('cuda:' + args.CUDA_VISIBLE_DEVICES[0] if torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+# print(device)
 
-fix_seed = 2021
-random.seed(fix_seed)
-torch.manual_seed(fix_seed)
-np.random.seed(fix_seed)
+if fixed_seed is not None:
+    random.seed(fixed_seed)
+    torch.manual_seed(fixed_seed)
+    np.random.seed(fixed_seed)
 
 if platform.system() == 'Windows':
     data_root = 'E:\\forecastdataset\\pkl'
@@ -62,6 +69,8 @@ data_preprocessor = None
 dataset = None
 if dataset_name == 'wht':
     dataset = pickle.load(open(os.path.join(data_root, 'wht.pkl'), 'rb'))
+elif dataset_name == 'gweather':
+    dataset = pickle.load(open(os.path.join(data_root, 'weather.pkl'), 'rb'))
 else:
     print('\033[32mno such dataset\033[0m')
     exit()
@@ -77,7 +86,12 @@ test_loader = DataLoader(TSDataset(test_input, test_gt), batch_size=batch_size, 
 
 model = None
 if which_model == 'linear':
-    model = LSTFLinear(input_size=input_len, output_size=output_len, sensors=num_sensors, individual=individual)
+    model = LTSFLinear(input_size=input_len, output_size=output_len, sensors=num_sensors, individual=individual)
+elif which_model == 'nlinear':
+    model = LTSFNLinear(input_size=input_len, output_size=output_len, sensors=num_sensors, individual=individual)
+elif which_model == 'dlinear':
+    model = LTSFDLinear(input_size=input_len, output_size=output_len, sensors=num_sensors, individual=individual,
+                        kernel_size=kernel_size)
 else:
     print('\033[32mno such model\033[0m')
     exit()
@@ -88,6 +102,7 @@ loss_fn = torch.nn.MSELoss()
 pbar_epoch = tqdm(total=total_eopchs, ascii=True, dynamic_ncols=True)
 minium_loss = 100000
 validate_loss_list = []
+last_save_step = -1
 for epoch in range(total_eopchs):
     # train
     model.train()
@@ -125,9 +140,12 @@ for epoch in range(total_eopchs):
     pbar_epoch.set_postfix_str('validate_loss:{:.4f}'.format(validate_loss))
     pbar_epoch.update(1)
     if validate_loss < minium_loss:
+        last_save_step = epoch
         minium_loss = validate_loss
         torch.save(model.state_dict(), os.path.join(save_dir, 'best_model.pth'))
         pbar_epoch.set_description_str('saved at epoch %d %.4f' % (epoch + 1, minium_loss))
+    if epoch - last_save_step > 10:
+        break
 pbar_epoch.close()
 
 # test
@@ -135,6 +153,9 @@ pbar_iter = tqdm(total=len(test_loader), ascii=True, dynamic_ncols=True)
 pbar_iter.set_description_str('testing')
 output_list = []
 gt_list = []
+if best_model:
+    model.load_state_dict(torch.load(os.path.join(save_dir, 'best_model.pth')))
+model.eval()
 for i, (input, ground_truth) in enumerate(test_loader):
     input = input.to(device)
     output = model(input)
@@ -144,5 +165,6 @@ for i, (input, ground_truth) in enumerate(test_loader):
 pbar_iter.close()
 output_list = torch.concatenate(output_list, dim=0)
 gt_list = torch.concatenate(gt_list, dim=0)
-test_loss = loss_fn(output_list,gt_list).item()
-print('\033[32mtest loss:{:.4f}\033[0m'.format(test_loss))
+test_loss = loss_fn(output_list, gt_list).item()
+mae_loss = torch.mean(torch.abs(output_list - gt_list))
+print('\033[32mmse loss:{:.4f} mae loss:{:.4f}\033[0m'.format(test_loss, mae_loss))
