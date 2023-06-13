141b2cc64f944895ae2dc052f97f1f86f6eae41f
17110 <1711042219@qq.com>
2023-06-13 17:19:37 +0800

    fixed some bugs

--- shdir/baseline.sh
+++ shdir/baseline.sh
@@ -16,11 +16,12 @@ epoch=20
 mem_scalar=1
 cuda_device=0,1,2,3,4,5,6,7
 dev_num=8
-exec_date=23.6.13.1.worst.model
+exec_date=23.6.14.worst.model
+out_list="20 40 80 160 320 480"
 
 for model in crossformer informer autoformer fedformer pyraformer transformer reformer; do
     for dataset in gweather etth1 etth2 ettm1 ettm2 exchange; do
-        for output_len in 24 48 96 192 360 720; do
+        for output_len in $out_list; do
             if [ $model = "reformer" ]; then
                 batch_size=$(expr 30 \* $mem_scalar)
             elif [ $model = 'informer' ]; then
@@ -41,30 +42,30 @@ for model in crossformer informer autoformer fedformer pyraformer transformer re
             $exec
         done
     done
-    dataset=ill
-    for output_len in 24 36 48 60; do
-        if [ $model = "reformer" ]; then
-            batch_size=$(expr 30 \* $mem_scalar)
-        elif [ $model = 'informer' ]; then
-            batch_size=$(expr 40 \* $mem_scalar)
-        elif [ $model = 'autoformer' ]; then
-            batch_size=$(expr 25 \* $mem_scalar)
-        elif [ $model = 'fedformer' ]; then
-            batch_size=$(expr 90 \* $mem_scalar)
-        elif [ $model = 'pyraformer' ]; then
-            batch_size=$(expr 200 \* $mem_scalar)
-        elif [ $model = 'transformer' ]; then
-            batch_size=$(expr 10 \* $mem_scalar)
-        elif [ $model = 'crossformer' ]; then
-            batch_size=$(expr 15 \* $mem_scalar)
-        fi
-        exec="torchrun --nproc_per_node=$dev_num --nnodes=1 informermain.py -GDMC $cuda_device -e $epoch -o $output_len -b $batch_size --fixed_seed 3407 -m $model -d $dataset -S save/$exec_date/$model/$dataset/$output_len"
-        echo "$exec"
-        $exec
-    done
+#    dataset=ill
+#    for output_len in 24 36 48 60; do
+#        if [ $model = "reformer" ]; then
+#            batch_size=$(expr 30 \* $mem_scalar)
+#        elif [ $model = 'informer' ]; then
+#            batch_size=$(expr 40 \* $mem_scalar)
+#        elif [ $model = 'autoformer' ]; then
+#            batch_size=$(expr 25 \* $mem_scalar)
+#        elif [ $model = 'fedformer' ]; then
+#            batch_size=$(expr 90 \* $mem_scalar)
+#        elif [ $model = 'pyraformer' ]; then
+#            batch_size=$(expr 200 \* $mem_scalar)
+#        elif [ $model = 'transformer' ]; then
+#            batch_size=$(expr 10 \* $mem_scalar)
+#        elif [ $model = 'crossformer' ]; then
+#            batch_size=$(expr 15 \* $mem_scalar)
+#        fi
+#        exec="torchrun --nproc_per_node=$dev_num --nnodes=1 informermain.py -GDMC $cuda_device -e $epoch -o $output_len -b $batch_size --fixed_seed 3407 -m $model -d $dataset -S save/$exec_date/$model/$dataset/$output_len"
+#        echo "$exec"
+#        $exec
+#    done
 done
 
-exec_date=23.6.13.1
+exec_date=23.6.14
 
 for model in crossformer informer autoformer fedformer pyraformer transformer reformer; do
     for dataset in gweather etth1 etth2 ettm1 ettm2 exchange; do
@@ -89,25 +90,25 @@ for model in crossformer informer autoformer fedformer pyraformer transformer re
             $exec
         done
     done
-    dataset=ill
-    for output_len in 24 36 48 60; do
-        if [ $model = "reformer" ]; then
-            batch_size=$(expr 30 \* $mem_scalar)
-        elif [ $model = 'informer' ]; then
-            batch_size=$(expr 40 \* $mem_scalar)
-        elif [ $model = 'autoformer' ]; then
-            batch_size=$(expr 25 \* $mem_scalar)
-        elif [ $model = 'fedformer' ]; then
-            batch_size=$(expr 90 \* $mem_scalar)
-        elif [ $model = 'pyraformer' ]; then
-            batch_size=$(expr 200 \* $mem_scalar)
-        elif [ $model = 'transformer' ]; then
-            batch_size=$(expr 10 \* $mem_scalar)
-        elif [ $model = 'crossformer' ]; then
-            batch_size=$(expr 15 \* $mem_scalar)
-        fi
-        exec="torchrun --nproc_per_node=$dev_num --nnodes=1 informermain.py -GBDMC $cuda_device -e $epoch -o $output_len -b $batch_size --fixed_seed 3407 -m $model -d $dataset -S save/$exec_date/$model/$dataset/$output_len"
-        echo "$exec"
-        $exec
-    done
+#    dataset=ill
+#    for output_len in 24 36 48 60; do
+#        if [ $model = "reformer" ]; then
+#            batch_size=$(expr 30 \* $mem_scalar)
+#        elif [ $model = 'informer' ]; then
+#            batch_size=$(expr 40 \* $mem_scalar)
+#        elif [ $model = 'autoformer' ]; then
+#            batch_size=$(expr 25 \* $mem_scalar)
+#        elif [ $model = 'fedformer' ]; then
+#            batch_size=$(expr 90 \* $mem_scalar)
+#        elif [ $model = 'pyraformer' ]; then
+#            batch_size=$(expr 200 \* $mem_scalar)
+#        elif [ $model = 'transformer' ]; then
+#            batch_size=$(expr 10 \* $mem_scalar)
+#        elif [ $model = 'crossformer' ]; then
+#            batch_size=$(expr 15 \* $mem_scalar)
+#        fi
+#        exec="torchrun --nproc_per_node=$dev_num --nnodes=1 informermain.py -GBDMC $cuda_device -e $epoch -o $output_len -b $batch_size --fixed_seed 3407 -m $model -d $dataset -S save/$exec_date/$model/$dataset/$output_len"
+#        echo "$exec"
+#        $exec
+#    done
 done
