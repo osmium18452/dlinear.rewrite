49c84130b61e245c6866365e60a4d71e5a697396
17110 <1711042219@qq.com>
2023-06-18 22:16:17 +0800

    fixed some bugs

--- process_save.py
+++ process_save.py
@@ -1,13 +1,13 @@
 import os
 import json
 
-root_dir = 'save/23.6.16'
+root_dir = 'save/23.6.17/not.ind'
 count = 0
 result_list = []
-with open('save/23.6.16.csv', 'w') as f:
+with open('save/23.6.17.not.ind.csv', 'w') as f:
     for root, dir_list, file_list in os.walk(root_dir):
         for file in file_list:
-            result = json.loads(open(os.path.join(root, file)).read())
+            result = json.loads(open(os.path.join(root, file),encoding='utf8').read())
             result_list.append({
                 'lr': result['lr'],
                 'output_len': result['output_len'],
@@ -18,5 +18,6 @@ with open('save/23.6.16.csv', 'w') as f:
                   result['output_len'],
                   result['mse'],
                   result['mae'],
+                  result['save_step'],
                   file=f,
                   sep=',')
