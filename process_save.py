6061c22ed79b519b4f1699ea65a0e4d6ec1edc88
17110 <1711042219@qq.com>
2023-08-10 20:40:10 +0800

    fixed some bugs

--- process_save.py
+++ process_save.py
@@ -1,23 +1,9 @@
-import os
 import json
+import os
+import pickle
 
-root_dir = 'save/23.6.17/not.ind'
-count = 0
-result_list = []
-with open('save/23.6.17.not.ind.csv', 'w') as f:
-    for root, dir_list, file_list in os.walk(root_dir):
-        for file in file_list:
-            result = json.loads(open(os.path.join(root, file),encoding='utf8').read())
-            result_list.append({
-                'lr': result['lr'],
-                'output_len': result['output_len'],
-                'mse': result['mse'],
-                'mae': result['mae'],
-            })
-            print(result['lr'],
-                  result['output_len'],
-                  result['mse'],
-                  result['mae'],
-                  result['save_step'],
-                  file=f,
-                  sep=',')
+for root, data_list, file_list in os.walk('save/finance.sup/23.8.9'):
+    for file in file_list:
+        if file.endswith('.json'):
+            result = json.load(open(os.path.join(root, file), encoding='utf8'))
+            print(result['model'], result['output_len'], result['mse'], result['mae'], sep=',')
