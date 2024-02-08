3c3ad30b5caa32fa93483e102a1d2c9f4fe9ad08
os <1711042219@qq.com>
2024-02-08 12:17:56 +0800

    added some data

--- process_save.py
+++ process_save.py
@@ -2,8 +2,8 @@ import json
 import os
 import pickle
 
-for root, data_list, file_list in os.walk('save/finance.sup/23.8.9'):
+for root, data_list, file_list in os.walk(r'E:\Pycharm Projects\dlinear.rewrite\save\24.2.5\flinear'):
     for file in file_list:
         if file.endswith('.json'):
             result = json.load(open(os.path.join(root, file), encoding='utf8'))
-            print(result['model'], result['output_len'], result['mse'], result['mae'], sep=',')
+            print(result['dataset'], result['output_len'], result['mse'], result['mae'], sep=',')
